9ed963526bd364a0e15272f1c5045c7d0aafb993
wuliangyp <163101596+wuliangyp@users.noreply.github.com>
2024-03-13 16:15:05 +0800

    Update WalineComponent.js 增加const locale = {

--- components/WalineComponent.js
+++ components/WalineComponent.js
@@ -12,7 +12,7 @@ let waline = null
  * @returns
  */
 const WalineComponent = (props) => {
-  const containerRef = createRef() //原本无React.，据【https://github.com/lifeafter619/NotionNext/blob/6062ddd6b6b3fc7e3901e7edec65e4ff116208e1/components/WalineComponent.js#L30】改
+  const containerRef = createRef() 
   const router = useRouter()
 
   const updateWaline = url => {
@@ -20,14 +20,21 @@ const WalineComponent = (props) => {
       waline.update(props)
     }
   }
-  //const locale = {
-  //  placeholder: "何事沉吟？", //评论框占位提示符，默认'欢迎评论'
-  //  sofa: "你好探险家，这里是人迹罕至的评论区大陆。",
-  //  reaction0: "give me five",
-    //reaction1: "鼓个掌",
-    //reaction2: "不赞成",
-    //reaction3: "祝好运",
-  //}
+  const locale = {
+    nick: '昵称',
+    nickError: '昵称不能少于3个字符哦...',
+    mail: '邮箱',
+    mailError: '请填写正确的邮件地址',
+    link: '您的网址',
+    optional: '可选',
+    placeholder: "何事沉吟？", //评论框占位提示符，默认'欢迎评论'
+    sofa: "你好探险家，这里是人迹罕至的评论区大陆。",
+    reactionTitle: '击掌！',
+    reaction0: "give me five",
+    reaction1: "鼓个掌",
+    reaction2: "不赞成",
+    reaction3: "祝好运",
+  }
 
   React.useEffect(() => {
     if (!waline) {
@@ -36,15 +43,8 @@ const WalineComponent = (props) => {
         el: containerRef.current,
         serverURL: siteConfig('COMMENT_WALINE_SERVER_URL'),
         lang: siteConfig('LANG'),
-        locale: {
-          placeholder: "何事沉吟？", //评论框占位提示符，默认'欢迎评论'
-          sofa: "你好探险家，这里是人迹罕至的评论区大陆。",
-          reactionTitle: "(〃￣︶￣)人(￣︶￣〃)",
-          reaction0: "give me five",
-          reaction1: "鼓个掌",
-          reaction2: "不赞成",
-          reaction3: "祝好运",
-        },
+        locale,
+        reaction: true,
         reaction: [ // 评论区反应功能，默认为reaction: true,
           'https://cdn-icons-png.flaticon.com/128/8208/8208813.png', // give me five
           'https://cdn-icons-png.flaticon.com/128/8208/8208756.png', // 鼓个掌
@@ -63,6 +63,7 @@ const WalineComponent = (props) => {
           "https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/tw-emoji",
           "https://cdn.jsdelivr.net/gh/norevi/waline-blobcatemojis@1.0/blobs",
           "https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/bilibili",
+          '//file.66619.eu.org/beluga-emoji',
         ], //自定义表情包
       });
     }
