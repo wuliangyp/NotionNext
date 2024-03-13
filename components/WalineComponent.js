030337941c23171242bedbe849e742d93002618d
wuliangyp <163101596+wuliangyp@users.noreply.github.com>
2024-03-14 01:19:58 +0800

    Update WalineComponent.js 回到原點

--- components/WalineComponent.js
+++ components/WalineComponent.js
@@ -20,7 +20,6 @@ const WalineComponent = (props) => {
       waline.update(props)
     }
   }
-
   React.useEffect(() => {
     if (!waline) {
       waline = init({
@@ -34,12 +33,7 @@ const WalineComponent = (props) => {
           'https://cdn.jsdelivr.net/gh/norevi/waline-blobcatemojis@1.0/blobs',
           'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/bilibili'
         ],
-        reaction: [
-          'https://cdn-icons-png.flaticon.com/128/8208/8208813.png',
-          'https://cdn-icons-png.flaticon.com/128/8208/8208756.png',
-          'https://cdn-icons-png.flaticon.com/128/8208/8208902.png',
-          'https://cdn-icons-png.flaticon.com/128/8208/8208766.png'
-        ]
+        reaction: false
       })
     }
 
