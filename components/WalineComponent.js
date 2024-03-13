41a1952d1ce6b53c20537615ad6f76bca9151e84
wuliangyp <163101596+wuliangyp@users.noreply.github.com>
2024-03-14 01:02:48 +0800

    Update WalineComponent.js 去掉  const locale = { 放入inti

--- components/WalineComponent.js
+++ components/WalineComponent.js
@@ -20,21 +20,6 @@ const WalineComponent = (props) => {
       waline.update(props)
     }
   }
-  const locale = {
-    nick: '昵称',
-    nickError: '昵称不能少于3个字符哦...',
-    mail: '邮箱',
-    mailError: '请填写正确的邮件地址',
-    link: '您的网址',
-    optional: '可选',
-    placeholder: "何事沉吟？", //  评论框占位提示符，默认'欢迎评论'
-    sofa: "你好探险家，这里是人迹罕至的评论区大陆。",
-    reactionTitle: '击掌！',
-    reaction0: "give me five",
-    reaction1: "鼓个掌",
-    reaction2: "不赞成",
-    reaction3: "祝好运",
-  }
 
   React.useEffect(() => {
     if (!waline) {
@@ -43,6 +28,21 @@ const WalineComponent = (props) => {
         el: containerRef.current,
         serverURL: siteConfig('COMMENT_WALINE_SERVER_URL'),
         lang: siteConfig('LANG'),
+        locale {
+          nick: '昵称',
+          nickError: '昵称不能少于3个字符哦...',
+          mail: '邮箱',
+          mailError: '请填写正确的邮件地址',
+          link: '您的网址',
+          optional: '可选',
+          placeholder: "何事沉吟？", //  评论框占位提示符，默认'欢迎评论'
+          sofa: "你好探险家，这里是人迹罕至的评论区大陆。",
+          reactionTitle: '击掌！',
+          reaction0: "give me five",
+          reaction1: "鼓个掌",
+          reaction2: "不赞成",
+          reaction3: "祝好运",
+        },
         reaction: [ //  评论区反应功能，默认为reaction: true,
           'https://cdn-icons-png.flaticon.com/128/8208/8208813.png', //  give me five
           'https://cdn-icons-png.flaticon.com/128/8208/8208756.png', //  鼓个掌
