5f423eee8fd22b5e7cfe2497770ac84faff9a247
wuliangyp <163101596+wuliangyp@users.noreply.github.com>
2024-03-14 14:41:35 +0800

    Update WalineComponent.js 刪除bilibili表情emoji

--- components/WalineComponent.js
+++ components/WalineComponent.js
@@ -30,8 +30,7 @@ const WalineComponent = (props) => {
         dark: 'html.dark',
         emoji: [
           'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/tw-emoji',
-          'https://cdn.jsdelivr.net/gh/norevi/waline-blobcatemojis@1.0/blobs',
-          'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/bilibili'
+          'https://cdn.jsdelivr.net/gh/norevi/waline-blobcatemojis@1.0/blobs'
         ],
         reaction: [
           'https://cdn-icons-png.flaticon.com/128/8208/8208784.png',
