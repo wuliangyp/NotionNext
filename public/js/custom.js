c76890e9402f0800b6078a17a030d3083ca5ec84
wuliangyp <163101596+wuliangyp@users.noreply.github.com>
2024-03-15 15:52:50 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,11 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+var formAction = document.querySelector('form').getAttribute('action');
+
+var aTags = document.querySelectorAll('.notion-gallery-grid a');
+
+aTags.forEach(function(aTag) {
+  aTag.closest('.notion-gallery-grid').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.open(formAction);
+  });
+});
